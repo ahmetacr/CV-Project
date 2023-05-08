59b19dbda683b386ba0b308807e1a01b9aceb977
ahmetacr <ahmetacar1111@gmail.com>
2023-05-08 07:10:49 +0300

    Add main component to the App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 // import "./index.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Main from "./components/MainPage/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
