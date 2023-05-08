4ecb0d1d65ab76484849208fd9ce18cf02a74706
ahmetacr <ahmetacar1111@gmail.com>
2023-05-08 07:10:20 +0300

    Init the main component

---
+++ src/components/MainPage/Main.js
@@ -0,0 +1,10 @@
+import "../../styles/Main.css";
+import { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return <div className="main"></div>;
+  }
+}
+
+export default Main;
