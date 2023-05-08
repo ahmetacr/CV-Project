347533d0a01ac734d4859a96335b9ded7cabd70d
ahmetacr <ahmetacar1111@gmail.com>
2023-05-08 07:11:12 +0300

    Delete the unnecessary constructor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,6 @@ import { Component } from "react";
 import "../styles/Footer.css";
 
 class Footer extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="footer">
