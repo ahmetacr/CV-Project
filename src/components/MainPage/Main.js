bdaa9057d31e5702ef944b71338dd71d3d43e23a
ahmetacr <ahmetacar1111@gmail.com>
2023-05-09 11:18:18 +0300

    create the main component to handle to store the states

--- src/components/MainPage/Main.js
+++ src/components/MainPage/Main.js
@@ -1,12 +1,67 @@
 import "../../styles/Main.css";
 import { Component } from "react";
 import InputPage from "./Inputs/InputPage";
+import OutputPage from "./Outputs/OutputPage";
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      generalInfo: {
+        name: "name",
+        email: "email",
+        phoneNumber: "phoneNumber",
+      },
+      educationInfo: {
+        schoolName: "schoolName",
+        major: "major",
+        completionDate: "compDate",
+      },
+      experienceInfo: {
+        companyName: "compName",
+        title: "title",
+        mainTasks: "mainTasks",
+        startDate: "startDare",
+        endDate: "endDate",
+      },
+    };
+    this.updateGeneralInfo = this.updateGeneralInfo.bind(this);
+    this.updateEducationInfo = this.updateEducationInfo.bind(this);
+  }
+
+  updateGeneralInfo(nameValue, emailValue, phoneNumberValue) {
+    this.setState((prevState) => ({
+      generalInfo: {
+        ...prevState.generalInfo,
+        name: nameValue,
+        email: emailValue,
+        phoneNumber: phoneNumberValue,
+      },
+    }));
+  }
+
+  updateEducationInfo(schoolNameValue, majorValue, completionDateValue) {
+    this.setState((prevState) => ({
+      educationInfo: {
+        ...prevState.educationInfo,
+        schoolName: schoolNameValue,
+        major: majorValue,
+        completionDate: completionDateValue,
+      },
+    }));
+  }
+
+  handleSubmit(e) {}
+
   render() {
     return (
       <div className="main">
-        <InputPage />
+        <InputPage
+          handleSubmit={this.handleSubmit}
+          updateGeneralInfo={this.updateGeneralInfo}
+          updateEducationInfo={this.updateEducationInfo}
+        />
+        <OutputPage info={this.state} />
       </div>
     );
   }
