e0fa4b5c6e1a54800085dae0a5a2d46854756b60
ahmetacr <ahmetacar1111@gmail.com>
2023-05-11 10:18:11 +0300

    update the layout to make style appealing

--- src/components/MainPage/Outputs/OutputPage.js
+++ src/components/MainPage/Outputs/OutputPage.js
@@ -7,7 +7,7 @@ class OutputPage extends Component {
     return (
       <div className="outputPage">
         <div className="outputPageHeader">
-          <h2>{info.generalInfo.name} Resume</h2>
+          <h2>{info.generalInfo.name}</h2>
           <div className="headerInfoSection">
             <p>{info.generalInfo.email}</p>
             <p>{info.generalInfo.phoneNumber}</p>
@@ -15,21 +15,27 @@ class OutputPage extends Component {
         </div>
         <div className="resumeContainer">
           <section>
+            <h1 className="sectionTitle">Education</h1>
             {info.educationInfoArr.map((item, index) => (
-              <div key={index} className="educationOutput">
-                <h2 id="shoolNameID">{item.schoolName}</h2>
-                <h2 id="majorID">{item.major}</h2>
-                <h2 id="completionDateID">Graduated: {item.completionDate}</h2>
+              <div key={index} className="educationOutput item">
+                <div className="educationContainer">
+                  <h3>{item.schoolName}</h3>
+                  <h4>{item.major}</h4>
+                </div>
+                <p>Graduated: {item.completionDate}</p>
               </div>
             ))}
+            <h1 className="sectionTitle">Experience</h1>
             {info.experienceInfoArr.map((item, index) => (
-              <div key={index} className="experienceOutput">
-                <h1 className="experienceOutputHeader">Experience</h1>
-                <h2 id="companyNameID">{item.companyName}</h2>
-                <h2 id="titleID">{item.title}</h2>
-                <h2 id="mainTasksID">{item.mainTasks}</h2>
-                <h2 id="startDateID">Worked from: {item.startDate}</h2>
-                <h2 id="endDateID">Worked to: {item.endDate}</h2>
+              <div key={index} className="experienceOutput item">
+                <div className="experienceContainer">
+                  <h3>{item.companyName}</h3>
+                  <h4>{item.title}</h4>
+                </div>
+                <p>Main tasks: {item.mainTasks}</p>
+                <p>
+                  Worked from: {item.startDate} to: {item.endDate}
+                </p>
               </div>
             ))}
           </section>
