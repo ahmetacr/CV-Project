5b8eaad9ec37a46c8d6d32ff37d86591c3a0c762
ahmetacr <ahmetacar1111@gmail.com>
2023-05-11 09:06:48 +0300

    add updateExperienceInfo method

--- src/components/MainPage/Inputs/InputPage.js
+++ src/components/MainPage/Inputs/InputPage.js
@@ -5,18 +5,15 @@ import EducationInfo from "./EducationInfo";
 import "../../../styles/InputPage.css";
 
 class InputPage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <>
         <form className="inputPageForm">
           <GeneralInfo updateGeneralInfo={this.props.updateGeneralInfo} />
           <EducationInfo updateEducationInfo={this.props.updateEducationInfo} />
-          <ExperienceInfo />
-          {/* <button type="submit"> SUBMIT </button> */}
+          <ExperienceInfo
+            updateExperienceInfo={this.props.updateExperienceInfo}
+          />
         </form>
       </>
     );
