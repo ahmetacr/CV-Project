b6fb632df1f100ace863677ae0ea18f293d04544
ahmetacr <ahmetacar1111@gmail.com>
2023-05-11 08:41:14 +0300

    create and implement getInfo to simplify the project

--- src/components/MainPage/Inputs/EducationInfo.js
+++ src/components/MainPage/Inputs/EducationInfo.js
@@ -13,77 +13,69 @@ class EducationInfo extends Component {
     };
   }
 
-  getSchoolName = (e) => {
+  getInfo = (e, key) => {
     this.setState({
-      schoolName: e.target.value,
-    });
-  };
-
-  getMajor = (e) => {
-    this.setState({
-      major: e.target.value,
-    });
-  };
-
-  getCompletionDate = (e) => {
-    this.setState({
-      completionDate: e.target.value,
+      [key]: e.target.value,
     });
   };
 
   addEducationInfo = (e) => {
-    e.preventDefault();
     const educationInfoObj = this.state;
     this.props.updateEducationInfo(educationInfoObj);
+
+    // Reset the state to clear the input fields
+    this.setState({
+      schoolName: "",
+      major: "",
+      completionDate: "",
+    });
   };
 
   render() {
     const { schoolName, major, completionDate } = this.state;
 
     return (
-      <form onSubmit={this.addEducationInfo}>
-        <div className="EducationInfo">
-          <h2>Education Information</h2>
-          <div className="schoolNameSection">
-            <InputField
-              label="School Name: "
-              type="text"
-              name="schoolNameInput"
-              id="schoolNameInput"
-              value={schoolName}
-              onChange={this.getSchoolName}
-              placeholder="Your School's Name"
-            />
-          </div>
-          <div className="majorSection">
-            <InputField
-              label="Major: "
-              type="text"
-              name="majorInput"
-              id="majorInput"
-              value={major}
-              onChange={this.getMajor}
-              placeholder="Your Major"
-            />
-          </div>
-          <div className="completionDateSection">
-            <InputField
-              label="Completion Date: "
-              type="date"
-              name="completionDateInput"
-              id="completionDateInput"
-              value={completionDate}
-              onChange={this.getCompletionDate}
-            />
-          </div>
-          <button
-            className="addBtn addEducationBtn"
-            onClick={this.addEducationInfo}
-          >
-            ADD
-          </button>
+      <div className="EducationInfo">
+        <h2>Education Information</h2>
+        <div className="schoolNameSection">
+          <InputField
+            label="School Name: "
+            type="text"
+            name="schoolNameInput"
+            id="schoolNameInput"
+            value={schoolName}
+            onChange={(e) => this.getInfo(e, "schoolName")}
+            placeholder="Your School's Name"
+          />
         </div>
-      </form>
+        <div className="majorSection">
+          <InputField
+            label="Major: "
+            type="text"
+            name="majorInput"
+            id="majorInput"
+            value={major}
+            onChange={(e) => this.getInfo(e, "major")}
+            placeholder="Your Major"
+          />
+        </div>
+        <div className="completionDateSection">
+          <InputField
+            label="Completion Date: "
+            type="date"
+            name="completionDateInput"
+            id="completionDateInput"
+            value={completionDate}
+            onChange={(e) => this.getInfo(e, "completionDate")}
+          />
+        </div>
+        <button
+          className="addBtn addEducationBtn"
+          onClick={this.addEducationInfo}
+        >
+          ADD
+        </button>
+      </div>
     );
   }
 }
